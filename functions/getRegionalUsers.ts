8db050757bcf11e4f5dbc7b7c32e0e9c60f05a90
Coordenador LC <coord.lc@e-vias.com>
2026-01-26 16:34:54 +0000

    File changes

--- functions/getRegionalUsers.ts
+++ functions/getRegionalUsers.ts
@@ -1,4 +1,4 @@
-import { createClientFromRequest } from 'npm:@base44/sdk@0.7.1';
+import { createClientFromRequest } from 'npm:@base44/sdk@0.8.6';
 
 /**
  * Função de backend para listar usuários filtrados por regional.
