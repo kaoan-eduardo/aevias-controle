fb1750e051800f027805941192d7416b891ba5a9
Coordenador LC <coord.lc@e-vias.com>
2026-02-19 17:42:47 +0000

    File changes

--- src/components/relatorios/RelatorioDiario.jsx
+++ src/components/relatorios/RelatorioDiario.jsx
@@ -418,7 +418,7 @@ export default function RelatorioDiario({ diario, obra, project, user, regional 
           <table className="w-full border-collapse text-sm" style={{ borderColor: 'hsl(212.73deg 26.83% 83.92%)' }} border="1">
             <thead>
               <tr className="bg-gray-50">
-                <th className="border border-slate-900 p-2 text-center font-bold" colSpan="4">EFETIVO DE COLABORADORES</th>
+                <th className="border p-2 text-center font-bold" style={{ borderColor: 'hsl(212.73deg 26.83% 83.92%)' }} colSpan="4">EFETIVO DE COLABORADORES</th>
               </tr>
             </thead>
             <tbody>
