7343ad5c328972ddc4bbfdec99291d90c27a9c45
Coordenador LC <coord.lc@e-vias.com>
2026-02-26 19:23:06 +0000

    File changes

--- src/pages/RelatorioVigaBenkelman.jsx
+++ src/pages/RelatorioVigaBenkelman.jsx
@@ -548,7 +548,7 @@ export default function RelatorioVigaBenkelman() {
               )}
             </div>
           </div>
-            </footer>
+              </footer>
             </div>
           );
         })}
