20b7ddcd97f91c184fb151eddd7823ea3e7d3df4
Coordenador LC <coord.lc@e-vias.com>
2026-03-31 10:40:15 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -517,7 +517,7 @@ const AppLayout = ({ children }) => {
   return (
     <SidebarProvider>
       <Dialog open={isCreateEnsaioOpen} onOpenChange={setIsCreateEnsaioOpen}>
-        <div className="min-h-screen flex w-full bg-[#F2F1EF]">
+        <div className="min-h-screen flex w-full bg-[#F2F1EF] overflow-x-hidden">
           <Sidebar className="border-r border-white/10 bg-white/10 backdrop-blur-xl">
             <SidebarHeader className="border-b border-white/10 p-4">
               <div className="flex items-center justify-center">
@@ -852,7 +852,7 @@ const AppLayout = ({ children }) => {
             </AlertDialog>
           </Sidebar>
 
-          <main className="flex-1 flex flex-col">
+          <main className="flex-1 flex flex-col min-w-0 overflow-x-hidden">
             {/* Mobile unified header */}
             <div
               className="lg:hidden fixed top-0 left-0 right-0 z-40 bg-[#F2F1EF]/95 backdrop-blur-md border-b border-black/10 flex items-center justify-between px-4"
@@ -891,7 +891,7 @@ const AppLayout = ({ children }) => {
                 className="flex-1 flex flex-col"
               >
                 <PullToRefresh>
-                  <div className="pb-16 lg:pb-0 lg:pt-0" style={{ paddingTop: 'calc(3rem + env(safe-area-inset-top))' }}>
+                  <div className="pb-16 lg:pb-0 lg:pt-0 overflow-x-hidden" style={{ paddingTop: 'calc(3rem + env(safe-area-inset-top))' }}>
                     {children}
                   </div>
                 </PullToRefresh>
